f9598661de0affcae926c230df9895c01c1b8f8f
major-NEELKAMAL <addressneel@gmail.com>
2026-08-28 13:58:30 +0530

    using ssr

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://supplychain-api-3ntq.onrender.com/api/v1' // Or your full production API endpoint URL, e.g., 'https://api.yourdomain.com/api/v1'
+  apiUrl: 'https://supplychain-api-3ntq.onrender.com/api/v1'
 };
